72ea70cb519d8cdd8c6fe47a6c6e2247dc492cf3
Francisco Passos <passos.27@hotmail.com>
2017-08-18 13:16:28 -0700

    testando

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -89,11 +89,11 @@
     </div>
 </div>
 
-<script type="text/javascript" src="{{asset('js/materialize.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery.mask.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/funcoes.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/cropper.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery.maskMoney.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/materialize.min.js')}}"></script>
 @yield('scripts')
 </body>
 </html>
