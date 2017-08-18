72d88d888c837631b6a626ee12ecf708e6752e14
Francisco Passos <passos.27@hotmail.com>
2017-08-17 22:25:02 -0700

    commit

--- resources/views/admin/relatorios/index_ganhos_inst.blade.php
+++ resources/views/admin/relatorios/index_ganhos_inst.blade.php
@@ -1,9 +1,9 @@
 @extends('layout')
 @section('content')
     @include('admin.menu')
-    <div class="container">
-    	<h2>Ganhos por Instituição:</h2>
-    	<h3>Selecione as datas</h3>
+    <div class="container top__cem">
+        <h3 class="grey-text">Ganhos por <strong>Instituição</strong></h3>
+    	<h4 class="grey-text">Selecione as datas</h4>
         @if($errors->any())
             <ul class="alert">
                 @foreach($errors->all() as $erro)
@@ -13,13 +13,13 @@
         @endif
     	{!! Form::open(['route'=>'ganhos.por.inst','method'=>'get'])!!}
 
-    	<div class="row l12">	
-    	 <div class="col s4">
+    	<div class="row">
+    	 <div class="col s6">
             {!! Form::label('dt_inicio','Data de inicío:') !!}
             {!! Form::date('dt_inicio', null, ['placeholder'=>'Data de inicio']) !!}
         </div>
 
-        <div class="col s4">
+        <div class="col s6">
             {!! Form::label('dt_fim','Data de fim:') !!}
             {!! Form::date('dt_fim', null, ['placeholder'=>'Data de fim']) !!}
         </div>
