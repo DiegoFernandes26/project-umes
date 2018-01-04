80a8d28db96a65e7022ff0e9b6d225d73e77dd7b
Francisco Passos <passos.27@hotmail.com>
2018-01-03 19:47:38 -0800

    commit ver carteira

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
